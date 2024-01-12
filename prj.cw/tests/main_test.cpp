7172e4b25222ec8fdeefe3c6dda7c681a41a6436
due <im_trying@mail.ru>
2024-01-12 19:52:51 +0300

    Added cxxopts to handle weird stuff

--- prj.cw/tests/main_test.cpp
+++ prj.cw/tests/main_test.cpp
@@ -1,9 +1,69 @@
-﻿#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+﻿//#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#define DOCTEST_CONFIG_NO_UNPREFIXED_OPTIONS
+#define DOCTEST_CONFIG_IMPLEMENT
+
 #include <doctest/doctest.h>
 #include <loader/loader.hpp>
 #include <algorithm>
 #include <opencv2/highgui.hpp>
 #include <map>
+#include <cxxopts.hpp>
+
+
+class dt_removed {
+	std::vector<const char*> vec;
+public:
+	dt_removed(const char** argv_in) {
+		for (; *argv_in; ++argv_in)
+			if (strncmp(*argv_in, "--dt-", strlen("--dt-")) != 0)
+				vec.push_back(*argv_in);
+		vec.push_back(nullptr);
+	}
+
+	int          argc() { return static_cast<int>(vec.size()) - 1; }
+	const char** argv() { return &vec[0]; } // Note: non-const char **:
+};
+
+std::string location;
+int program(int argc, const char** argv);
+
+int main(int argc, const char** argv) {
+	doctest::Context context(argc, argv);
+
+
+
+	dt_removed args(argv);
+	try {
+		cxxopts::Options options("Unit Tests", "Unit tests for a weirdo");
+		options.add_options()
+			("f,file", "File name", cxxopts::value<std::string>()->default_value(CMAKE_TEST_PATH));
+		options.allow_unrecognised_options();
+		auto result = options.parse(args.argc(), args.argv());
+		location = result["file"].as<std::string>();
+		if (location.empty()) {
+			location = CMAKE_TEST_PATH;
+		}
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what();
+		return 1 ;
+	}
+
+	int test_result = context.run(); // run queries, or run tests unless --no-run
+
+	if (context.shouldExit()) // honor query flags and --exit
+		return test_result;
+
+	int app_result = program(args.argc(), args.argv());
+
+	return test_result + app_result; // combine the 2 results
+}
+
+int program(int argc, const char** argv) {
+	return EXIT_SUCCESS;
+}
+
 
 namespace ve {
 	class TestLoader final : public ILoader {
@@ -47,7 +107,7 @@ namespace {
 	(const std::vector<ve::Path>& answers, const std::filesystem::path& rel_test_path,
 		ve::DirectoryIteration it = ve::DirectoryIteration::NORMAL) -> bool
 		{
-			const std::filesystem::path test_dir_path = std::filesystem::path(CMAKE_TEST_PATH).string() + rel_test_path.string();
+			const std::filesystem::path test_dir_path = std::filesystem::path(location).string() + rel_test_path.string();
 			auto test_dir_entires = ve::listFiles(test_dir_path, it);
 			return check_if_everything_was_found_correctly(test_dir_entires, answers, test_dir_path);
 		};
@@ -64,7 +124,7 @@ TEST_CASE("List files") {
 			{"test_dir", normal_entries}
 		};
 
-		REQUIRE_NOTHROW(ve::listFiles(CMAKE_TEST_PATH));
+		REQUIRE_NOTHROW(ve::listFiles(location));
 
 		for (const auto& test_case : cases) {
 			CHECK(check_list_files(test_case.second, test_case.first));
@@ -73,7 +133,7 @@ TEST_CASE("List files") {
 	}
 
 	SUBCASE("Recursive") {
-		REQUIRE_NOTHROW(ve::listFiles(CMAKE_TEST_PATH));
+		REQUIRE_NOTHROW(ve::listFiles(location));
 		CHECK(check_list_files({"file.txt","test_rec_dir", "test_rec_dir/file2.txt"}, "test_dir", ve::DirectoryIteration::RECURSIVE));
 	}
 }
@@ -98,8 +158,8 @@ TEST_CASE("Image loader") {
 		CHECK_FALSE(image_loader.loadFromFile("./"));
 		CHECK(image_loader.loadFromFile("./").code == ve::ErrorCodes::UnsupportedExtension);
 		const std::vector<std::filesystem::path> files{
-			std::string(CMAKE_TEST_PATH) + "images/1.jpg",
-			std::string(CMAKE_TEST_PATH) + "images/3.png"
+			std::string(location) + "images/1.jpg",
+			std::string(location) + "images/3.png"
 		};
 		for (const auto& el : files) {
 			CHECK(image_loader.loadFromFile(el));
@@ -125,29 +185,29 @@ TEST_CASE("Directory Loader") {
 		
 		CHECK(dir_loader.loadFromDirectory("./"));
 		dir_loader.reset();
-		CHECK(dir_loader.loadFromDirectory(CMAKE_TEST_PATH));
+		CHECK(dir_loader.loadFromDirectory(location));
 		CHECK(dir_loader.copyData().size() == 0);
 		dir_loader.reset();
 		const std::vector<std::filesystem::path> files{
-			std::string(CMAKE_TEST_PATH) + "images/1.jpg",
-			std::string(CMAKE_TEST_PATH) + "images/3.png"
+			std::string(location) + "images/1.jpg",
+			std::string(location) + "images/3.png"
 		};
 
 		CHECK(dir_loader.loadFromFiles(files.cbegin(), files.cend()));
 		CHECK(dir_loader.copyData().size() == 2);
 		dir_loader.reset();
-		CHECK(dir_loader.loadFromDirectory(std::string(CMAKE_TEST_PATH) + "images/"));
+		CHECK(dir_loader.loadFromDirectory(std::string(location) + "images/"));
 		CHECK(dir_loader.copyData().size() == 2);
 		dir_loader.reset();
-		ve::Error err = dir_loader.loadFromDirectory(std::string(CMAKE_TEST_PATH) + "images/bugged_images");
+		ve::Error err = dir_loader.loadFromDirectory(std::string(location) + "images/bugged_images");
 		CHECK(!err);
-		CHECK(err.message == std::string(CMAKE_TEST_PATH) + "images/bugged_images\\1.jpg");
+		CHECK(err.message == std::string(location) + "images/bugged_images\\1.jpg");
 		CHECK(dir_loader.copyData().size() == 0);
 		dir_loader.reset();
 
 		SUBCASE("DirectoryLoading, [Load a big amount of data]") {
 			CHECK(dir_loader.loadFromFiles(files.cbegin(), files.cend()));
-			const std::string big_data_path = std::string(CMAKE_TEST_PATH) + "/big_data/";
+			const std::string big_data_path = std::string(location) + "/big_data/";
 			const auto& data = dir_loader.getData();
 			ve::generateTestData(big_data_path, data[0]);
 			dir_loader.reset();
@@ -173,21 +233,21 @@ TEST_CASE("Directory Loader") {
 }
 
 TEST_CASE("ImageWriter") {
-	ve::ImageWriter writer(std::string(CMAKE_TEST_PATH) + "/test_masks/");
+	ve::ImageWriter writer(std::string(location) + "/test_masks/");
 	
 	ve::DirectoryLoader dir_loader;
 	
 	SUBCASE("Image Writer, [writing 1 cv::Mat to tests/masks]") {
 		const std::vector<std::filesystem::path> files{
-			std::string(CMAKE_TEST_PATH) + "images/1.jpg"
+			std::string(location) + "images/1.jpg"
 		};
 		dir_loader.loadFromFiles(files.cbegin(), files.cend());
 		CHECK(writer.saveMasks(dir_loader.getData().cbegin(), dir_loader.getData().cend()));
 	}
 	SUBCASE("Image Writer, [writing 2 cv::Mat to tests/masks]") {
 		const std::vector<std::filesystem::path> files{
-			std::string(CMAKE_TEST_PATH) + "images/1.jpg",
-			std::string(CMAKE_TEST_PATH) + "images/3.png"
+			std::string(location) + "images/1.jpg",
+			std::string(location) + "images/3.png"
 		};
 		dir_loader.loadFromFiles(files.cbegin(), files.cend());
 		const auto& data = dir_loader.getData();
